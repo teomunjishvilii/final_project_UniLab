1d3d82b0955455cb6898a431c42aa71076ff1c68
teomunjishvilii <teona.munjishvili.1@iliauni.edu.ge>
2021-08-06 15:57:12 +0400

    add slider and blocks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
+// Burger Button - the navigation appears by clicking on the burger button.
 let nav = document.getElementById('nav');
 let burgerButton = document.getElementById('burgerButton');
 
 burgerButton.addEventListener ('click', function () {
     nav.classList.toggle('active');
+});
+
+//Slider
+$ (document).ready(function() {
+    $('.slider').slick ({
+        dots: true,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 1000
+    });
 });
