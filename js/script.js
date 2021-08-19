29185b0a02700d95c44434c26a7bd0e1de85d298
teomunjishvilii <teona.munjishvili.1@iliauni.edu.ge>
2021-08-19 17:34:38 +0400

    add countdown

--- js/script.js
+++ js/script.js
@@ -16,3 +16,25 @@ $ (document).ready(function() {
         autoplaySpeed: 2000
     });
 });
+
+// Countdown
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    let event = "Sep 10, 2021 23:00:00",
+        countDown = new Date(event).getTime(),
+        x = setInterval(function() {    
+  
+          let now = new Date().getTime(),
+              distance = countDown - now;
+  
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+        }, 0)
+}());
